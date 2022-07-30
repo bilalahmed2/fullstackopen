239dc32f7853de38b40cd95b4aed65d99f073486
Bilal <imaddesire@gmail.com>
2022-07-31 01:40:31 +0500

    completed total. used reduce method

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,12 +5,15 @@ const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
 
+const Total = ({parts}) => <p><strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong></p>
+
 
 const Course = ({ course }) => {
   return (
     <>
       <Header heading={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
