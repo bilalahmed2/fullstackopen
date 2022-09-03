309efaebd980a1075d2fe407ae170e2c1230a4cd
Bilal <imaddesire@gmail.com>
2022-09-04 04:16:31 +0500

    Completed Exercises 4.1.-4.2.

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,17 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogRouter.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  }).catch(error => next(error));
+});
+
+
+blogRouter.get("/", (request, response) => {
+    Blog.find({}).then(blogs => response.json(blogs))
+})
+
+module.exports = blogRouter;
