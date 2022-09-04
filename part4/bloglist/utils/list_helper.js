0883c5f6b84994add2b7f38a328c52461784a2ff
Bilal <imaddesire@gmail.com>
2022-09-05 01:47:40 +0500

    ex: 4.5 completed

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,10 +3,19 @@ const dummy = (blog) => {
 }
 
 const totalLikes = (blog) => {
-  return blog.reduce((sum, likes) => {return sum + likes.likes}, 0 )
+  return blog.reduce((sum, likes) => {
+    return sum + likes.likes
+  }, 0)
+}
+
+const favoriteBlog = (blog) => {
+  return blog.reduce((firstElement, secondElement) =>
+    firstElement.likes > secondElement.likes ? firstElement : secondElement
+  )
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
